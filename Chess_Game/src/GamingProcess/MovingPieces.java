a214dc1da4e29d950eeed0a31df079dfc3ca1cc7
Gleison <gleisonmorais06@gmail.com>
2025-08-22 21:35:44 -0400

    Feat: MovingPiece get position

--- Chess_Game/src/GamingProcess/MovingPieces.java
+++ Chess_Game/src/GamingProcess/MovingPieces.java
@@ -1,13 +1,51 @@
 package GamingProcess;
+import java.util.Map;
 
 public class MovingPieces {
 
-    final private String pieceMoving;
+    private String fromPiece;
+    private String toPiece;
 
-    public MovingPieces( String pieceMoving ) { this.pieceMoving = pieceMoving; }
+    private int fromPieceidx;
+    private int toPieceidx;
 
-    public String getPieceMoving() {
-        return pieceMoving;
+    public MovingPieces(String fromPiece, String toPiece) { this.fromPiece = fromPiece; this.toPiece = toPiece; }
+
+    public int getToPieceidx() { return toPieceidx; }
+    public void setToPieceidx(int toPieceidx) { this.toPieceidx = toPieceidx; }
+
+    public int getFromPieceidx() { return fromPieceidx; }
+    public void setFromPieceidx(int fromPieceidx) { this.fromPieceidx = fromPieceidx; }
+
+    final public int[] StringToIndex(String alph, int i) {
+        Map<String, Integer> stringConvertFromStringToIndex = Map.of(
+                "a", 0,
+                "b", 1,
+                "c", 2,
+                "d", 3,
+                "e", 4,
+                "f", 5,
+                "g", 6,
+                "h", 7
+        );
+
+        Map<Integer, Integer> intConvertFromStringToIndex = Map.of(
+                8, 0,
+                7, 1,
+                6, 2,
+                5, 3,
+                4, 4,
+                3, 5,
+                2, 6,
+                1, 7
+        );
+
+        int[] positions = new int[2];
+        positions[0] = stringConvertFromStringToIndex.get(alph);
+        positions[1] = intConvertFromStringToIndex.get(i);
+
+        return positions;
+
     }
 
 }
--- Chess_Game/src/Main.java
+++ Chess_Game/src/Main.java
@@ -1,6 +1,7 @@
 import BoardChess.Board_Default;
 import BoardChess.Mount_Chess;
 import java.util.Scanner;
+import GamingProcess.MovingPieces;
 
 
 public class Main {
@@ -9,14 +10,19 @@ public class Main {
 
         Board_Default chess = new Mount_Chess().Mount_Chess_Default();
         Scanner sc = new Scanner(System.in);
-
         final boolean winOrLoser = true;
 
         while(winOrLoser) {
             System.out.println(chess);
-            break;
-        }
 
+            System.out.print("Movimento (ex: e2 e4): ");
+            String fromString = sc.next();
+            String toString = sc.next();
+
+//            MovingPieces position = new MovingPieces.StringToIndex(fromString.indexOf(0));
+
+
+        }
 
     }
 
